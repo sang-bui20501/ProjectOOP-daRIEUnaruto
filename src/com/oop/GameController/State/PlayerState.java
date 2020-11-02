2244c9d0d8cf5bfc31f5e260eb07b1f26c61ea67
Cong Trung <42043537+congtrung2k1@users.noreply.github.com>
2020-11-02 11:04:13 +0700

    11/2/2020

--- src/com/oop/GameController/State/PlayerState.java
+++ src/com/oop/GameController/State/PlayerState.java
@@ -2,9 +2,24 @@ package com.oop.GameController.State;
 
 import com.oop.GameController.Player.Player;
 
-public class PlayerState {
-    private Player p;
-    public PlayerState(Player p){
-        this.p = p;
-    }
+public abstract class PlayerState {
+    private String ip;
+    private int sessionID, health , mana , shield;
+    
+	public abstract void initInstance(int heatlh , int mana);
+    
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public int getSessionID() {
+		return sessionID;
+	}
+	public void setSessionID(int sessionID) {
+		this.sessionID = sessionID;
+	}
+    	
+    
 }
